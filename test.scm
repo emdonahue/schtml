6f6198c4f7f92c715e46bdb41c4b455b7a79bf1e
Evan Donahue <emdonahu@gmail.com>
2022-11-26 17:40:36 +0900

    removed subdirs

---
+++ test.scm
@@ -0,0 +1,23 @@
+(import (schtml))
+
+(define (test actual expected)
+  (if (not (equal? actual expected))
+	(display (string-append "expected: " expected "\nreceived: " actual "\n\n"))))
+
+(test (html "lorem ipsum") "lorem ipsum")
+(test (html '(p "lorem ipsum")) "<p>lorem ipsum</p>")
+(test (html '("p" "lorem ipsum")) "<p>lorem ipsum</p>")
+(test (html '((p class article) "lorem ipsum")) "<p class=\"article\">lorem ipsum</p>")
+(test (html '(p.article "lorem ipsum")) "<p class=\"article\">lorem ipsum</p>")
+(test (html '(p.article.main "lorem ipsum")) "<p class=\"article main\">lorem ipsum</p>")
+(test (html '((p class (bold italics)) "lorem ipsum")) "<p class=\"bold italics\">lorem ipsum</p>")
+(test (html '(p (span "lorem") " " (span "ipsum"))) "<p><span>lorem</span> <span>ipsum</span></p>")
+
+(test (css '((.bold font-weight bold))) ".bold{font-weight:bold;}")
+(test (css '((.bold font-weight bold) (.italics font-style italic))) ".bold{font-weight:bold;}.italics{font-style:italic;}")
+(test (css '(("p > span" font-weight bold))) "p > span{font-weight:bold;}")
+(test (css '(((p > span) font-weight bold))) "p > span{font-weight:bold;}")
+(test (css '((.border border (1px solid black)))) ".border{border:1px solid black;}")
+
+
+(display "Testing Complete")
